84d543a893f749dce04b6ef0fcc9c2e394a66467
Jason Mei <jason.mei403@gmail.com>
2018-02-03 17:06:28 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase, dafault="Squawk!")
+def parrot(dafault="Squawk!")
   puts phrase
   return phrase
 end
